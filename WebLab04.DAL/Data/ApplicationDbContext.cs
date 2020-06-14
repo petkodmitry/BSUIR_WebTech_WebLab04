529ca1d9488341ecb7df0e0e95c994c0f65874d4
petkodmitry <petkodmitry@gmail.com>
2020-06-14 17:50:10 +0300

    init commit

---
+++ WebLab04.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebLab04.DAL.Entities;
+
+namespace WebLab04.DAL.Data
+{
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+
+        }
+    }
+}
---
+++ WebLab04.DAL/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebLab04.DAL.Entities
+{
+	public class ApplicationUser : IdentityUser {
+	}
+}
